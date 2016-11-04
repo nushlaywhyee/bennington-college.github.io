a3a97053b0607302dbc7f51d3eeeba44110dffbb
nushlaywhyee <student@bcdm52461.local>
2016-11-04 11:46:16 -0400

    grey

--- assets/script.js
+++ assets/script.js
@@ -2,7 +2,7 @@ $( document ).ready(function() {
 
 	$('div').hover(
 	  function() {
-	    $('div').css('background-color', 'yellow');
+	    $('div').css('background-color', 'grey');
 	    $('span').css('body-color', 'skyblue');
 	  }
 	);
